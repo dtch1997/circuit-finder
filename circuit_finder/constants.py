f6db56971ee54e571ad843d08a1d950f2fe1efef
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-05-25 14:08:49 +0000

    fix circular import

--- circuit_finder/constants.py
+++ circuit_finder/constants.py
@@ -1,9 +1,8 @@
 import torch
 import pathlib
-from circuit_finder.core.types import LayerIndex
 
 GPT_2_SMALL: str = "gpt2"
-ALL_GPT_2_SMALL_LAYERS: list[LayerIndex] = list(range(12))
+ALL_GPT_2_SMALL_LAYERS: list[int] = list(range(12))
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
